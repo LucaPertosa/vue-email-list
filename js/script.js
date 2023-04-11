d8798cb7d0e4a62bff7f2770592c1a79ece5d194
LucaPertosa <luca.pertosa@hotmail.it>
2023-04-11 15:41:03 +0200

    Complete vue email list + bonus

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            emailList: [],
+            isLoading: true
+        };
+    },
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            axios
+            .get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((resp) => {
+                if (!this.emailList.includes(resp.data.response)) {
+                    this.emailList.push(resp.data.response)
+                }
+                if (i === 9) {
+                    this.isLoading = false
+                }
+            })
+    }},
+}).mount("#app");
